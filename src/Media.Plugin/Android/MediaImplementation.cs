00979e16ee6f8239ed0cb0d3acab4a0b819dee2c
Robin Remedio <robin@levelapp.be>
2018-06-08 17:39:58 +0800

    debug 3

--- src/Media.Plugin/Android/MediaImplementation.cs
+++ src/Media.Plugin/Android/MediaImplementation.cs
@@ -447,23 +447,23 @@ namespace Plugin.Media
 
 					}
 
-					//if (cameraOptions.FlashMode == CameraFlash.On)
-					//{
-					//	pickerIntent.PutExtra(MediaPickerActivity.ExtraFlashON, true);
-					//	pickerIntent.PutExtra(MediaPickerActivity.ExtraFlashOFF, false);
-					//	pickerIntent.PutExtra("android.intent.extras.FLASH_MODE_ON", (int)CameraFlash.On);
-					//}
-					//else if (cameraOptions.FlashMode == CameraFlash.Off)
-					//{
-					//	pickerIntent.PutExtra(MediaPickerActivity.ExtraFlashON, false);
-					//	pickerIntent.PutExtra(MediaPickerActivity.ExtraFlashOFF, true);
-					//	pickerIntent.PutExtra("android.intent.extras.FLASH_MODE_ON", (int)CameraFlash.On);
-					//}
-					//else if (cameraOptions.FlashMode == CameraFlash.Default)
-					//{
-					//	pickerIntent.PutExtra(MediaPickerActivity.ExtraFlashAuto, true);
+					if (cameraOptions.FlashMode == CameraFlash.On)
+					{
+						pickerIntent.PutExtra(MediaPickerActivity.ExtraFlashON, true);
+						pickerIntent.PutExtra(MediaPickerActivity.ExtraFlashOFF, false);
+						pickerIntent.PutExtra("android.intent.extras.FLASH_MODE_ON", (int)CameraFlash.On);
+					}
+					else if (cameraOptions.FlashMode == CameraFlash.Off)
+					{
+						pickerIntent.PutExtra(MediaPickerActivity.ExtraFlashON, false);
+						pickerIntent.PutExtra(MediaPickerActivity.ExtraFlashOFF, true);
+						pickerIntent.PutExtra("android.intent.extras.FLASH_MODE_ON", (int)CameraFlash.On);
+					}
+					else if (cameraOptions.FlashMode == CameraFlash.Default)
+					{
+						pickerIntent.PutExtra(MediaPickerActivity.ExtraFlashAuto, true);
 
-					//}
+					}
 
 					pickerIntent.PutExtra(MediaPickerActivity.ExtraSaveToAlbum, cameraOptions.SaveToAlbum);
                 }
